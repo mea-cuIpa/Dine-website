f9e0e7ff995162b31fa937e86105d98e316e9c56
mea-cuIpa <juchnevic98@outlook.com>
2021-02-04 20:51:06 +0200

    init

---
+++ JS/app.js
@@ -0,0 +1,44 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const people_count = document.getElementById("people");
+  const counter_container = document.querySelector(".counter");
+  let count = parseInt(people_count.value);
+  let updateCount = () => (people_count.value = count);
+
+  counter_container.addEventListener("click", (event) => {
+    event.preventDefault();
+    const element = event.target;
+    if (element.nodeName === "BUTTON") {
+      if (element.id === "inc" && count < 20) count++;
+      else if (element.id === "dec" && count > 2) count--;
+    }
+    updateCount();
+  });
+
+  const ampmList = document.querySelector(".ampm__list");
+  const ampmInput = document.getElementById("ampm_input");
+
+  ampmList.addEventListener("click", (event) => {
+    event.preventDefault();
+    const element = event.target;
+    [...ampmList.children].forEach((listItem) =>
+      listItem.classList.remove("selected")
+    );
+    element.classList.add("selected");
+    ampmInput.value = element.innerText;
+  });
+
+  const inputGroups = document.querySelectorAll(".input-group");
+  const submit_btn = document.querySelector('button[type="submit"]');
+
+  submit_btn.addEventListener("click", (event) => {
+    event.preventDefault();
+    [...inputGroups].forEach((inputGroup) => {
+      const inputValues = Array.from(
+        inputGroup.querySelectorAll("input"),
+        (input) => input.value
+      );
+      if (inputValues.includes("")) inputGroup.classList.add("error");
+      else inputGroup.classList.remove("error");
+    });
+  });
+});
